24fc45ebc98094d725f9f7265cbdea3f821b4a23
Grant Royseth <groyseth@gmail.com>
2022-03-31 11:45:02 -0500

    changed connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/google-booksearch', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://groyseth:ginger6686@cluster0.89osj.mongodb.net/bookSearch?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
