3ffcaf8b4b93b3a4a112f636e81f7a692a787385
Grant Royseth <groyseth@gmail.com>
2022-03-31 11:27:31 -0500

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://groyseth:ginger6686@cluster0.89osj.mongodb.net/google-booksearch?retryWrites=true&w=majority', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
